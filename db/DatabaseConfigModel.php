c2dae98c459ec0c2ed4406817c432f7581795d59
Tiziano Cocciò <tiziano.coccio@gmail.com>
2014-03-30 22:28:30 +0200

    added a model of the database configuration file

---
+++ db/DatabaseConfigModel.php
@@ -0,0 +1,13 @@
+<?php
+# Rename this file to DatabaseConfig.php
+class DatabaseConfig {
+    public $default = array(
+        'datasource' => 'Database/Mysql',
+        'persistent' => true,
+        'host' => 'localhost',
+        'login' => 'username',
+        'password' => 'password',
+        'database' => 'db-name',
+        'prefix' => '');
+}
+?>
