0e42cfa8679ef89e40d5a0e112776e271be7fb1c
Tiziano Cocciò <tiziano.coccio@gmail.com>
2014-04-11 00:26:37 +0200

    cleaning folder

--- app/configurationModel.js
+++
@@ -1,5 +0,0 @@
-// Rename this file to configuration.js
-angular.module('configuration', [])
-       .constant('CONFIGURATION', {
-       		root: '/'
-       	});
--- boot.php
+++
@@ -1,7 +0,0 @@
-<?php
-define ("PERCORSO_IMMAGINI", __DIR__ . '/data/immagini/locandine/');
-
-
-include ("db/Database.php");
-$db = Database::Load();
-?>
